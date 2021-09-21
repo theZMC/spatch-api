37201f42591167b109bdaa5f6e32c3c6d3c5f06a
Zach Callahan <zach@zmc.dev>
2021-09-21 17:49:31 -0500

    wrapped technician controller in a response builder

--- src/main/java/app/spatch/controller/TechnicianController.java
+++ src/main/java/app/spatch/controller/TechnicianController.java
@@ -1,5 +1,6 @@
 package app.spatch.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import app.spatch.model.Technician;
@@ -12,32 +13,49 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/technicians")
 @Produces(MediaType.APPLICATION_JSON)
 public class TechnicianController {
   private TechnicianService service = new TechnicianService();
 
+
+  private Response buildResponse(List<Technician> technicians){
+    Response response = Response
+    .status(200)
+    .entity(technicians)
+    .header("Access-Control-Allow-Origin", "*")
+    .build();
+
+    return response;
+  }
+  private Response buildResponse(Technician technician){
+    List<Technician> technicians = new ArrayList<Technician>();
+    technicians.add(technician);
+    return buildResponse(technicians);
+  }
+
   @GET
-  public List<Technician> getTechnicians(){
-    return service.getTechnicians();
+  public Response getTechnicians(){
+    return buildResponse(service.getTechnicians());
   }
 
   @GET
   @Path("/{id}")
-  public List<Technician> getTechnicianById(@PathParam("id") Integer id){
-    return service.getTechnicianById(id);
+  public Response getTechnicianById(@PathParam("id") Integer id){
+    return buildResponse(service.getTechnicianById(id));
   }
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
-  public Technician createTechnician(Technician technician){
-    return service.createTechnician(technician);
+  public Response createTechnician(Technician technician){
+    return buildResponse(service.createTechnician(technician));
   }
 
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
-  public Technician updateTechnician(Technician technician){
-    return service.updateTechnician(technician);
+  public Response updateTechnician(Technician technician){
+    return buildResponse(service.updateTechnician(technician));
   }
 }
